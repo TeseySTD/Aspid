a11e683c6e08e128a484968e002d0a515591693e
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2026-01-31 11:09:17 +0200

    feat:Add base evaluator.

--- src/Aspid.Cli/Program.cs
+++ src/Aspid.Cli/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Aspid.Core.Binding;
+using Aspid.Core.Evaluator;
 using Aspid.Core.Syntax;
 
-Binder binder = new Binder(new());
+var variables = new Dictionary<string, TypeSymbol>();
+var values = new Dictionary<string, object>();
+
+var binder = new Binder(variables);
+var evaluator = new Evaluator(values);
 
 while (true)
 {
@@ -15,7 +20,42 @@ while (true)
 
     Parser parser = new Parser(input);
     var statements = parser.Parse();
-    var boundNodes = statements.Select(binder.Bind);
-    foreach (var node in boundNodes)
-        Binder.PrettyPrint(node, isFirst: true);
+    
+    
+    foreach (var statement in statements)
+    {
+        binder.Diagnostics.Clear();
+        
+        var boundNode = binder.Bind(statement);
+
+        if (binder.Diagnostics.Any())
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            foreach (var diagnostic in binder.Diagnostics)
+            {
+                Console.WriteLine(diagnostic);
+            }
+            Console.ResetColor();
+        }
+        else
+        {
+            try 
+            {
+                var result = evaluator.Evaluate(boundNode);
+                
+                if (result != null && boundNode.Type != TypeSymbol.Void)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(result);
+                    Console.ResetColor();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Runtime Error: {e.Message}");
+                Console.ResetColor();
+            }
+        }
+    }
 }
---
+++ src/Aspid.Core/Evaluator/Evaluator.cs
@@ -0,0 +1,121 @@
+﻿using Aspid.Core.Binding;
+
+namespace Aspid.Core.Evaluator;
+
+public class Evaluator
+{
+    private readonly Dictionary<string, object> _variables;
+
+    public Evaluator(Dictionary<string, object> variables)
+    {
+        _variables = variables;
+    }
+
+    public object? Evaluate(BoundNode node)
+    {
+        return node switch
+        {
+            // Statements
+            BoundVariableDeclarationStatement v => EvaluateVariableDeclaration(v),
+            BoundAssignmentStatement a => EvaluateAssignment(a),
+            
+            // Expressions
+            BoundLiteralExpression l => l.Value,
+            BoundVariableExpression v => EvaluateVariableExpression(v),
+            BoundBinaryExpression b => EvaluateBinaryExpression(b),
+            
+            // Error Node
+            BoundErrorNode => null,
+
+            _ => throw new Exception($"Unexpected node {node.GetType().Name}")
+        };
+    }
+
+    private object? EvaluateVariableDeclaration(BoundVariableDeclarationStatement node)
+    {
+        var value = node.Initializer != null ? Evaluate(node.Initializer) : null;
+        
+        _variables[node.Variable.Name] = value!;
+        
+        return value;
+    }
+
+    private object EvaluateAssignment(BoundAssignmentStatement node)
+    {
+        var value = Evaluate(node.Expression);
+        _variables[node.Variable.Name] = value!;
+        return value!;
+    }
+
+    private object EvaluateVariableExpression(BoundVariableExpression node)
+    {
+        return _variables[node.Name];
+    }
+
+    private object EvaluateBinaryExpression(BoundBinaryExpression node)
+    {
+        var left = Evaluate(node.Left)!;
+        var right = Evaluate(node.Right)!;
+
+        return node.Op.Kind switch
+        {
+            BoundBinaryOperatorKind.Addition => EvaluateAddition(node, left, right),
+            BoundBinaryOperatorKind.Subtraction => EvaluateSubtraction(node, left, right),
+            BoundBinaryOperatorKind.Multiplication => EvaluateMultiplication(node, left, right),
+            BoundBinaryOperatorKind.Division => EvaluateDivision(node, left, right),
+            BoundBinaryOperatorKind.Equals => EvaluateEquals(left, right),
+            _ => throw new Exception($"Unexpected binary operator {node.Op.Kind}")
+        };
+    }
+
+    private object EvaluateAddition(BoundBinaryExpression node, object left, object right)
+    {
+        if (node.Type == TypeSymbol.String)
+        {
+            return Convert.ToString(left) + Convert.ToString(right);
+        }
+
+        if (node.Type == TypeSymbol.Double)
+        {
+            return Convert.ToDouble(left) + Convert.ToDouble(right);
+        }
+
+        return Convert.ToInt32(left) + Convert.ToInt32(right);
+    }
+
+    private object EvaluateSubtraction(BoundBinaryExpression node, object left, object right)
+    {
+        if (node.Type == TypeSymbol.Double)
+            return Convert.ToDouble(left) - Convert.ToDouble(right);
+        
+        return Convert.ToInt32(left) - Convert.ToInt32(right);
+    }
+
+    private object EvaluateMultiplication(BoundBinaryExpression node, object left, object right)
+    {
+        if (node.Type == TypeSymbol.Double)
+            return Convert.ToDouble(left) * Convert.ToDouble(right);
+        
+        return Convert.ToInt32(left) * Convert.ToInt32(right);
+    }
+
+    private object EvaluateDivision(BoundBinaryExpression node, object left, object right)
+    {
+        if (node.Type == TypeSymbol.Double)
+            return Convert.ToDouble(left) / Convert.ToDouble(right);
+        
+        return Convert.ToInt32(left) / Convert.ToInt32(right);
+    }
+
+    private object EvaluateEquals(object left, object right)
+    {
+        if (IsNumeric(left) && IsNumeric(right))
+        {
+            return Math.Abs(Convert.ToDouble(left) - Convert.ToDouble(right)) < double.Epsilon;
+        }
+
+        return Equals(left, right);
+    }
+
+    private static bool IsNumeric(object obj) => obj is int or double;
+}
