441f23ca26f238d9f99473d5771fc430040cda54
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2026-02-06 23:08:51 +0200

    fix:Fix return statement typing.

--- src/Aspid.Core/Binding/Statements.cs
+++ src/Aspid.Core/Binding/Statements.cs
@@ -1,96 +1,84 @@
 ﻿namespace Aspid.Core.Binding;
 
-public sealed class BoundBlockStatement(List<BoundNode> statements) : BoundNode
+public class BoundStatement : BoundNode
+{
+    public override TypeSymbol Type => TypeSymbol.Void;
+}
+
+public sealed class BoundBlockStatement(List<BoundNode> statements) : BoundStatement 
 {
     public List<BoundNode> Statements { get; } = statements;
-    public override TypeSymbol Type => TypeSymbol.Void;
 }
 
 public sealed class BoundVariableDeclarationStatement(BoundVariableExpression variable, BoundNode? initializer)
-    : BoundNode
+    : BoundStatement 
 {
     public BoundVariableExpression Variable { get; } = variable;
     public BoundNode? Initializer { get; } = initializer;
-    public override TypeSymbol Type => TypeSymbol.Void;
 }
 
-public sealed class BoundFunctionDeclarationStatement(FunctionSymbol function, BoundNode action) : BoundNode
+public sealed class BoundFunctionDeclarationStatement(FunctionSymbol function, BoundNode action) : BoundStatement
 {
     public FunctionSymbol Function { get; } = function;
     public BoundNode Action { get; } = action;
-    public override TypeSymbol Type => Function.Type;
 }
 
-public sealed class BoundAssignmentStatement(BoundVariableExpression variable, BoundNode expression) : BoundNode
+public sealed class BoundAssignmentStatement(BoundVariableExpression variable, BoundNode expression) : BoundStatement
 {
     public BoundVariableExpression Variable { get; } = variable;
     public BoundNode Expression { get; } = expression;
-
-    public override TypeSymbol Type => TypeSymbol.Void;
 }
 
 public sealed class BoundArrayAssignmentStatement(
     BoundArrayAccessExpression arrayAccess,
     BoundNode expression
-) : BoundNode
+) : BoundStatement
 {
     public BoundArrayAccessExpression ArrayAccess { get; } = arrayAccess;
     public BoundNode Expression { get; } = expression;
-
-    public override TypeSymbol Type => TypeSymbol.Void;
 }
 
 public sealed class BoundIfStatement(
     BoundNode condition,
     BoundNode thenStatement,
     BoundNode? elseStatement
-) : BoundNode
+) : BoundStatement
 {
     public BoundNode Condition { get; } = condition;
     public BoundNode ThenStatement { get; } = thenStatement;
     public BoundNode? ElseStatement { get; } = elseStatement;
-
-    public override TypeSymbol Type => TypeSymbol.Void;
 }
 
 public sealed class BoundWhileStatement(
     BoundNode condition,
     BoundNode actionStatement
-) : BoundNode
+) : BoundStatement
 {
     public BoundNode Condition { get; } = condition;
     public BoundNode ActionStatement { get; } = actionStatement;
-
-    public override TypeSymbol Type => TypeSymbol.Void;
 }
 
 public sealed class BoundDoWhileStatement(
     BoundNode condition,
     BoundNode actionStatement
-) : BoundNode
+) : BoundStatement
 {
     public BoundNode Condition { get; } = condition;
     public BoundNode ActionStatement { get; } = actionStatement;
-
-    public override TypeSymbol Type => TypeSymbol.Void;
 }
 
 public sealed class BoundForInStatement(
     BoundVariableDeclarationStatement variable,
     BoundNode enumerator,
     BoundNode actionStatement
-) : BoundNode
+) : BoundStatement
 {
     public BoundVariableDeclarationStatement Variable { get; } = variable;
     public BoundNode Enumerator { get; } = enumerator;
     public BoundNode ActionStatement { get; } = actionStatement;
-    public override TypeSymbol Type => TypeSymbol.Void;
 }
 
-public sealed class BoundReturnStatement(BoundNode? expression) : BoundNode
+public sealed class BoundReturnStatement(BoundNode? expression) : BoundStatement
 {
     public BoundNode? Expression { get; } = expression;
-    
-    public override TypeSymbol Type => Expression?.Type ?? TypeSymbol.Void;
-    
 }
